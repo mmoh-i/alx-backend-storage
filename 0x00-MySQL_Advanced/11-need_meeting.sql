e56153cc21c06d4d147342c6ba707b9d075c4c4d
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-19 09:00:34 +0100

    need_meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,3 +1,5 @@
+-- A SQL script that creates a view need_meeting that lists all students that have a score under 80 (stric)
+-- and no last_meeting or more than 1 month.   
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
