7a91893499c5bdd1d022f56b24f2af53094c1cf2
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-19 22:35:58 +0100

    8.all documents with python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+"""8. all documents at python. """
 
+
+def list_all(mongo_collection):
+    """ Lists all documents in a collection """
+    if mongo_collection is None:
+        return []
+    return mongo_collection.find()
